dcb918dcca8f8619d6c7388487d954c71c934c36
Wojciech Koszek <wojciech@koszek.com>
2017-05-22 22:52:51 -0700

    Remove junk from here.

--- bitfountain/bitfountain.rb
+++ bitfountain/bitfountain.rb
@@ -23,27 +23,3 @@ video_page = mechanize.get(page_url)
 pp(video_page)
 pp(video_page.frames)
 pp(video_page.iframes)
-
-
-#(1..count).each do |idx|
-#    puts "# #{idx}"
-#    begin
-#       page_url = "http://www.nsscreencast.com/episodes/#{idx}"
-#	video_page = mechanize.get(page_url)
-#	video_url = video_page.uri.to_s + ".mp4"
-#	pp(video_url)
-#
-#        puts "Downloading #{video_url}"
-#        video = mechanize.get(video_url)
-#
-#        file_name = video_url.split("/")[-1]
-#	file_path = "videos/#{file_name}"
-#	pp(file_name)
-#	pp(file_path)
-#
-#        video.save(file_path)
-#        puts "Saved #{file_path}"
-#    rescue
-#        puts "Couldn't save #{video_url}"
-#    end
-#end
